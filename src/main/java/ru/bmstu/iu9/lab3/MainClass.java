14d351df71534fe5543c99e6edc69be68386157e
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:25:37 +0300

    Scripted auto-commit on change (2020-12-20 20:25:37) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab3/MainClass.java
@@ -41,7 +41,7 @@ public class MainClass {
         JavaRDD<String> intputAirportRdd = context.textFile(airportPath).mapPartitionsWithIndex(removeHeaders, false);
         JavaRDD<String> intputFlightsRdd = context.textFile(flightsPath).mapPartitionsWithIndex(removeHeaders, false);
 
-        JavaPairRDD<Integer, String> airportNames = intputAirportRdd
+        JavaPairRDD<Integer, String> airportNames = intputAirportRdd.mapToPair(line -> )
     }
 
 }
