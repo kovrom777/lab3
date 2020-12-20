9e5b67b38b371e7e405788a63a1ca5c6d4116123
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:14:05 +0300

    Scripted auto-commit on change (2020-12-20 20:14:05) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/AirportDataSerializable.java
+++ src/main/java/ru/bmstu/iu9/lab3/AirportDataSerializable.java
@@ -8,6 +8,12 @@ public class AirportDataSerializable implements Serializable {
     private int originalAirportId, destinationAirportId;
     private double timeDaley;
     private boolean isCanceled;
-    
 
+    public AirportDataSerializable(int originalAirportId, int destinationAirportId, double timeDaley, boolean isCanceled) {
+        this.originalAirportId = originalAirportId;
+        this.destinationAirportId = destinationAirportId;
+        this.timeDaley = timeDaley;
+        this.isCanceled = isCanceled;
+    }
+    
 }
