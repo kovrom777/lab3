256820937c1c5a71dffa3550d0de4380cd4a1b27
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:51:56 +0300

    Scripted auto-commit on change (2020-12-20 20:51:56) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab3/MainClass.java
@@ -95,7 +95,7 @@ public class MainClass {
 
         JavaRDD<String> output = reducesRes.map(item -> {
            String tmpOutput = "";
-           tmpOutput += airportBroadcast.value().get(item._1._1) + " " + ;
+           tmpOutput += airportBroadcast.value().get(item._1._1) + " " + item._1._1;
            tmpOutput += airportBroadcast.value().get(item)
         });
 
