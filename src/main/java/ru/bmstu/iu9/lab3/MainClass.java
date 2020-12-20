079edc2a306bb573413b735e210fdb1656e110af
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:44:24 +0300

    Scripted auto-commit on change (2020-12-20 20:44:24) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab3/MainClass.java
@@ -82,7 +82,7 @@ public class MainClass {
         });
 
 
-        JavaPairRDD<Tuple2<Integer, Integer>, FlightsSerializable>
+        JavaPairRDD<Tuple2<Integer, Integer>, FlightsSerializable> reducesRes = resultRdd.
 
     }
 
