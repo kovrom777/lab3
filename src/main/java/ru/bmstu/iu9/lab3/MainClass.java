f85fd1cbb9c03de11c3bd2abd4177d9519ae791c
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:06:30 +0300

    Scripted auto-commit on change (2020-12-20 20:06:30) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab3/MainClass.java
@@ -1,5 +1,8 @@
 package ru.bmstu.iu9.lab3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class MainClass {
 
 
@@ -7,6 +10,15 @@ public class MainClass {
 
     public static void main(String[] args) {
 
+        if (args.length != 3){
+            System.out.println("Not enough arguments");
+        }
+
+        String airportPath = args[0];
+        String flightsPath = args[1];
+
+        SparkConf conf = new SparkConf().setAppName("example");
+        JavaSparkContext context = new JavaSparkContext(conf);
     }
 
 }
