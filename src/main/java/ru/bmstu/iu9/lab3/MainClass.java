32ee0ac4e7646dc9837a093f85f1bb3df4bbee09
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:07:45 +0300

    Scripted auto-commit on change (2020-12-20 20:07:45) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab3/MainClass.java
@@ -23,7 +23,7 @@ public class MainClass {
         SparkConf conf = new SparkConf().setAppName("example");
         JavaSparkContext context = new JavaSparkContext(conf);
 
-        Function2 removeHeaders = new Function2<Integer, Iterator<String>, Iterator<String>> {
+        Function2 removeHeaders = new Function2<Integer, Iterator<String>, Iterator<String>>() {
             @Override
             public Object call(Object o, Object o2) throws Exception {
                 return null;
