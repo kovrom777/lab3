f8e2cba1bf6dcde7a9611a34f2afa341fc202769
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:32:23 +0300

    Scripted auto-commit on change (2020-12-20 20:32:23) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab3/MainClass.java
@@ -55,7 +55,9 @@ public class MainClass {
                return new Tuple2<>(flightId, airport);
         });
 
-        Map<Integer, String> airName = airportNames
+        Map<Integer, String> airName = airportNames.collectAsMap();
+
+        
     }
 
 }
