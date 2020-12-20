eb5cbe595033e01f1659988369891540ac6c8866
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:30:50 +0300

    Scripted auto-commit on change (2020-12-20 20:30:50) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab3/MainClass.java
@@ -48,7 +48,7 @@ public class MainClass {
            for (int i = 1; i< airportArray.length; i++){
                airport += airportArray[i];
                String preParsedId = airportArray[0].replace(QUOTATION_MARK_CONSTANT, "");
-               int flightId = 
+               int flightId = Integer.parseInt()
            }
         });
     }
