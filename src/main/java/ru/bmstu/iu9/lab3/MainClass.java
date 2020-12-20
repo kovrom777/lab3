43fd360dd7ef9116f36212003d78f61e82b6cd8c
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:48:11 +0300

    Scripted auto-commit on change (2020-12-20 20:48:11) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab3/MainClass.java
@@ -90,6 +90,8 @@ public class MainClass {
             return new FlightsSerializable(p.getTimeDaley(), delayNumber, 1);
         }, FlightsSerializable::addValue, FlightsSerializable::add);
 
+        Ma
+
     }
 
 }
