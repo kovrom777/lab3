567d43b73a4d75df4547166ef11ea97df0e34cff
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:17:13 +0300

    Scripted auto-commit on change (2020-12-20 20:17:13) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/FlightsSerializable.java
+++ src/main/java/ru/bmstu/iu9/lab3/FlightsSerializable.java
@@ -41,6 +41,10 @@ public class FlightsSerializable {
         allFlightsNumber++;
     }
 
-    public void 
+    public void incrementFlightNumber(){
+        allFlightsNumber++;
+    }
+
+    
 
 }
