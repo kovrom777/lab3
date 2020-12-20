4818a70ed48f556d91bb95a1929a0ff2918a18d0
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:33:11 +0300

    Scripted auto-commit on change (2020-12-20 20:33:11) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab3/MainClass.java
@@ -57,7 +57,7 @@ public class MainClass {
 
         Map<Integer, String> airName = airportNames.collectAsMap();
 
-        JavaPairRDD<Tuple2<Integer, Integer>, AirportDataSerializable>
+        JavaPairRDD<Tuple2<Integer, Integer>, AirportDataSerializable> resultRdd = 
     }
 
 }
