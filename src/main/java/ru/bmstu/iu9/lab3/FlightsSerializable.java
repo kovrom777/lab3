8911849aa06dbe8d3319286130ddd23b7054f87e
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:15:48 +0300

    Scripted auto-commit on change (2020-12-20 20:15:48) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/FlightsSerializable.java
+++ src/main/java/ru/bmstu/iu9/lab3/FlightsSerializable.java
@@ -3,6 +3,13 @@ package ru.bmstu.iu9.lab3;
 public class FlightsSerializable {
 
     private double maxDelay;
-    private  int flightDelaysNumber, 
+    private  int flightDelaysNumber, allFlightsNumber;
 
+    public FlightsSerializable(double maxDelay, int flightDelaysNumber, int allFlightsNumber) {
+        this.maxDelay = maxDelay;
+        this.flightDelaysNumber = flightDelaysNumber;
+        this.allFlightsNumber = allFlightsNumber;
+    }
+
+    
 }
