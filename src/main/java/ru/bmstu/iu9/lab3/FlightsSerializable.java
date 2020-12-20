4898ecafa68282c80ddeebdf8980510672ae3118
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:22:18 +0300

    Scripted auto-commit on change (2020-12-20 20:22:18) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/FlightsSerializable.java
+++ src/main/java/ru/bmstu/iu9/lab3/FlightsSerializable.java
@@ -60,7 +60,7 @@ public class FlightsSerializable {
     public static FlightsSerializable add(FlightsSerializable a, FlightsSerializable b){
         return  new FlightsSerializable(
                 Math.max(a.maxDelay, b.maxDelay),
-                
+                a.getFlightDelaysNumber(), 
 
         );
     }
