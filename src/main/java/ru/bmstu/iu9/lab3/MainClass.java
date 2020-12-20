107f55ae0f4ee3cecf49c7b5180fd57abc51d70d
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 20:46:10 +0300

    Scripted auto-commit on change (2020-12-20 20:46:10) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab3/MainClass.java
@@ -85,7 +85,7 @@ public class MainClass {
         JavaPairRDD<Tuple2<Integer, Integer>, FlightsSerializable> reducesRes = resultRdd.combineByKey(p -> {
             int delayNumber = 0;
             if (p.getDestinationAirportId() > 0 || p.isCanceled()){
-                
+                delayNumber ++;
             }
         });
 
